c9dc6b254e84c08d7a4de2fff54471364baeee42
Jerrish Varghese <jerrish@edstem.com>
2023-07-12 17:14:12 +0530

    removed target folder

--- book-managment/book-managment/target/classes/data.sql
+++
@@ -1,8 +0,0 @@
-insert into book_details(id, title, author, isbn, publication_date)
-values(1, 'Harry Potter One', 'Aishu', 100001, current_date());
-insert into book_details(id, title, author, isbn, publication_date)
-values(2, 'Harry Potter Two', 'Aishu', 100002, current_date());
-insert into book_details(id, title, author, isbn, publication_date)
-values(3, 'Harry Potter Three', 'Aishu', 100003, current_date());
-insert into book_details(id, title, author, isbn, publication_date)
-values(4, 'Harry Potter Four', 'Aishu', 100004, current_date());
